14ee71d6f2d6e2a86122b52ba2c29cdd7eb01942
Anthony Taylor <ant.taylor2@gmail.com>
2024-11-14 00:08:30 -0500

    adding info modal functionality to movie card

--- frontend/src/components/movie-card.tsx
+++ frontend/src/components/movie-card.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./favorite-button";
+import useInfoModal from "@/hooks/useInfoModals";
+import { BiChevronDown } from "react-icons/bi";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -11,6 +13,9 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+
+  const { openModal } = useInfoModal();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -33,7 +38,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         <img
           className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]"
           src={data.thumbnailUrl}
-          alt="Thumbnail"
+          alt="Thumbnail" 
         />
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center p-3 gap-2">
@@ -44,6 +49,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BiChevronDown className="text-white group-hover/item:text-neutral-300"  size={30} />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2024</span>
